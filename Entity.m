f0f8fc694646453642fba9f5b482457922bc5583
Aashiq <aashiq.meeran@hispark.in>
2014-04-05 15:50:55 +0530

    Revert 9f3ff1d..ed07fc2
    This rolls back to commit 9f3ff1d86c0d77d11b13b48b422d7abc99eb0ac6.

---
+++ Entity.m
@@ -0,0 +1,19 @@
+//
+//  Entity.m
+//  CodeManiaCore
+//
+//  Created by BSA univ 2 on 27/03/14.
+//  Copyright (c) 2014 BSA univ 2. All rights reserved.
+//
+
+#import "Entity.h"
+
+
+@implementation Entity
+
+@dynamic qn1;
+@dynamic qn2;
+
+
+
+@end
